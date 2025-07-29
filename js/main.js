499e1b84c5f07aa69723d6cdf5a7d67facdd33b4
Tiarna Boal <tiarna.boal99@gmail.com>
2025-07-29 16:27:57 +1000

    added styling

---
+++ js/main.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+// BURGER MENU FUNCTION
+    const menuIcon = document.getElementById('menu-icon');
+    const navMenu = document.getElementById('nav-menu');
+    const navLinks = navMenu.querySelectorAll('a');
+    const body = document.body;
+
+    function toggleMenu() {
+        navMenu.classList.toggle('open');
+        menuIcon.classList.toggle('change');
+        body.classList.toggle('no-scroll');
+
+        // update aria-expanded attribute
+        const expanded = menuIcon.getAttribute('aria-expanded') === 'true';
+        menuIcon.setAttribute('aria-expanded', String(!expanded));
+    }
+
+    // toggleMenu function on click
+    menuIcon.addEventListener('click', toggleMenu);
+
+    // toggleMenu function for keyboard functionality
+    menuIcon.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter' || e.key === ' ') {
+            e.preventDefault();
+            toggleMenu();
+        }
+    });
+
+    // close menu when nav-link is clicked
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            if (navMenu.classList.contains('open')) {
+                toggleMenu();
+            }
+        });
+    });
+});
+
